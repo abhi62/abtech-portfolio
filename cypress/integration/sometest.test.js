b0d89de59cdeafd48a898632b5640a2d7eebd8db
abhi62 <abhi.leon.93@gmail.com>
2022-01-13 13:02:44 +0530

    some more edit in test case

--- cypress/integration/sometest.test.js
+++ cypress/integration/sometest.test.js
@@ -1,15 +1,16 @@
 /// <reference types="cypress" />
 
-describe('sample test', () => {
+describe("sample test", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/');
+    cy.visit("http://localhost:3000/");
   });
 
-  it('displays two todo items by default', () => {
+  it("displays two todo items by default", () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-    cy.get('.icon-down-circle').click();
-    cy.get('.button');
+    cy.get(".icon-down-circle").click();
+    cy.get(".button");
+    cy.get("#contactName").type("abhishek");
   });
 });
